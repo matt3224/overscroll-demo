8c8730cd06431646b2ee49e5bb941f908e2523ce
Matt Pilott <matt@creativelittledots.co.uk>
2021-09-01 10:44:40 +0100

    initial

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	let scrollY = 0
+	let innerHeight = 0
+	let outerHeight = 0
+	
+	$: scrollHeight = outerHeight - innerHeight - 50
+</script>
+<svelte:window bind:scrollY bind:innerHeight bind:outerHeight />
+<div>
+	scroll: {scrollY}<br>
+	height: {scrollHeight}<br>
+	overscroll: {scrollY > scrollHeight || scrollY < 0}
+</div>
+
+<style>
+	:global(body) {
+		height: 1000px
+	}
+	div {
+		position: sticky;
+		top: 0;
+	}
+</style>
